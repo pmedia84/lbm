03edf7b9185dcb433626645a526f256ae12094c3
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-11-15 09:50:00 +0000

    .

--- public/php/nav.inc.php
+++ public/php/nav.inc.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta property="og:title" content="Lashes Brows and Aesthetics - Beauty" />
 <meta property="og:description" content="Transform your eyelashes to be fuller, longer and curlier for a sensational look" />
-<meta property="og:image" content="/og-data/logo.png"/>
+<meta property="og:image" content="https://lashesandbrowsbymandi.co.uk/og-data/logo.png"/>
 <meta property="og:type" content="website" />
 
 
