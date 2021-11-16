effeb97abe350468d4d6a268d74f58fe787b6ea8
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-11-16 10:05:06 +0000

    move

---
+++ public_html/about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Lashes, Brows and Aesthetics: About Me</title>
+    <meta name="description" content="About me, I love to transform your look with my amazing Treatments.">
+    <?php include("php/nav.inc.php"); ?>
+
+    <header>
+        <section class="abouthero hero containerfw ">
+            <div class="container herogrid">
+                <h1 class="herotitle">About Me</h1>
+            </div>
+        </section>
+    </header>
+<section class="container banner">
+    <h1 class="bannertitle m-b-1">Some of the treatments I provide</h1>
+    <p class="bannertext m-b-0">Transform your look with some of my amazing treatments and get that WOW factor!</p>
+</section>
+   
+<section class="container p-top-0 p-bt-0 full-width">
+    
+        <div class="gridcontainer">
+            <img class="seconditem img-fit" src="img/hybridlashes.jpg" alt="">
+            <div class="p-1 firstitem textbox">
+                <h2 class="mt-b-1 textbox-title">Lashes</h2>
+                <p class="content mp-b-1 textbox-subtitle">Eyes Are The Window To Your Soul</p>
+                <p class="content mp-b-1 textbox-text">From Classic to Extreme Mega Volume, I bond one lash(Classic) or more lighter weight lashes, handmade into a fan (Russian) to your natrual lash, giving a fuller, darker efffect.
+The look can vary from subtle, being Classic to extremely dramatic with Extreme Mega Volume.
+</p>
+                <ul class="fa-ul content">
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Russian Volume Lashes</li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Hybrid Lashes</li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Fresh Infills</li>
+                </ul>
+            </div>
+        </div>
+
+    <div class="gridcontainer">
+            <img class="img-fit" src="img/hennabrow.jpg" alt="">
+            <div class="p-1 textbox">
+                <h2 class="mt-b-1 textbox-title">Brows</h2>
+                <p class="content mp-b-1 textbox-subtitle">If Eyes Are The Window To Your Soul, Then Brows Are The Frame To That Window</p>
+                <p class="content mp-b-1 textbox-text">From a simple wax & shape, to the ever popular Brow Lamination & Hennna Brow, all brow treatments are catered for.</p>
+                <ul class="fa-ul content textbox-text">
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Henna Brows</li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Wax Tint & Tidy</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="gridcontainer">
+            <img class="seconditem img-fit" src="img/dermaplaning.jpg" alt="">
+            <div class="p-1 firstitem textbox">
+                <h2 class="mt-b-1 textbox-title">Derma Planing</h2>
+                <p class="content mp-b-1 textbox-subtitle">Anti-aging Skincare treatments without the chemicals</p>
+                <p class="content mp-b-1 textbox-text">Is a method of exfoliation that uses a scalpel blade to remove the top layer of dulling, dead skin cells, to leave a brighter, smoother, rejuvinated complexion. It removes the velus "Peach Fuzz" hair, making way for a great base for make up.
+Finished alongside with a relaxing prescription facial using Eve Taylor Products, this is a lovely relaxing treatment.
+</p>
+                <ul class="fa-ul content textbox-text">
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Increase absorption of topical products</li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Immediatley diminishes the appearance of fine lines and wrinkles</li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Primes skin for chemical peel</li>
+                    <li><span class="fa-li"><i class="fas fa-check"></i></span>Creates healthier, more radiant appearance</li>
+                </ul>
+            </div>
+        </div>
+
+</section>
+   
+
+    <?php include("php/footer.inc.php"); ?>
---
+++ public_html/prices.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Lashes, Brows and Aesthetics: Price List</title>
+    <meta name="description" content="My Price List">
+    <?php include("php/nav.inc.php"); ?>
+    
+    <header>
+        <section class="pricehero hero containerfw ">
+            <div class="container herogrid">
+                <h1 class="herotitle">Price List</h1>
+                <p class="herotext">These are my current prices.</p>
+            </div>
+        </section>
+    </header>
+
+
+    <div class="container">
+        <div class="banner p-bt-1">
+            <h1 class="bannertitle">Price List</h1>
+            <h3 class="bannersubtitle">These are my most popular treatments</h3>
+            <p class="bannertext">If there is something you want and it is not shown here, please contact me to discuss your requirements</p>
+        </div>
+
+        <section class="pricelist">
+        <script id="timelyScript" src="//book.gettimely.com/widget/book-button-v1.5.js"></script>
+        <?php 
+    $db = new mysqli("localhost", "lashesandbrows", "LBM_2021", "lashesan_pricelist",);
+
+
+    $query = "SELECT id, name, description, subtitle, price, button FROM product ORDER BY id";
+    $result = $db->query($query);
+    while ($row = $result->fetch_assoc()) {
+        $id = $row['id'];
+        $name = $row['name'];
+        $description = $row['description'];
+        $price = $row['price'];
+        $subtitle = $row['subtitle'];
+        $button = $row['button'];
+        echo "
+        
+        
+        <div class='pricecard'>
+                <div class='pricecard-title'>
+                    <h1>$name</h1>
+                </div>
+                <div class='pricecard-body'>
+                    <h1 class='pricecard-price'>£$price</h1>
+                    <p class='pricecard-price-subtitle'>$subtitle</p>
+                    <p class='pricecard-text'>$description</p>
+                    <p class='pricecard-text'>$button</p>
+                </div>
+            </div>
+        
+        
+        
+        
+        \n";
+    }
+
+    
+
+    ?>
+        
+     
+           
+
+        </section>
+    </div>
+    <div class="container">
+        <div class="banner p-bt-1">
+           
+            
+            <p class="bannertext mt-b-1">Infills to be done within 3 weeks of having a full set and over 50% of lashes remaining</p>
+            <p class="bannertext">A £25 non refundable booking fee, is taken on the day of your patch test, but it is taken off the cost on the day of your treatment.</p>
+        </div>
+    </div>
+    <?php include("php/footer.inc.php"); ?>
