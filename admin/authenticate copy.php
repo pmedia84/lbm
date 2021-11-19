f3186988d9235c6dc0f0570d1dc5406e0e14aa34
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-11-19 16:15:08 +0000

    ...

--- admin/authenticate copy.php
+++ admin/authenticate copy.php
@@ -1,7 +1,7 @@
 
 
 <?php
-
+session_start();
 // Change this to your connection info.
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'lashesbr_admin';
@@ -40,7 +40,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
         if (password_verify($_POST['password'], $password)) {
             // Verification success! User has logged-in!
             // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-            session_start();
+            session_regenerate_id();
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
             $_SESSION['id'] = $id;
