327f61cb37330e48fd8a970ac52eae02b51acf05
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-11-19 16:19:44 +0000

    mmm

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,5 @@
-
-
- <?php include("nav-admin.inc.php"); ?>
- <?php
+<?php include("nav-admin.inc.php"); ?>
+<?php
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
 // If the user is not logged in redirect to the login page...
