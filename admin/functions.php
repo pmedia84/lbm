d6d47454750561e20e803597f1340bbaeb52c578
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-11-22 14:48:23 +0000

    .

--- admin/functions.php
+++ admin/functions.php
@@ -3,7 +3,7 @@ function pdo_connect_mysql() {
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'lashesbr_admin';
     $DATABASE_PASS = 'LBA_2021!';
-    $DATABASE_NAME = 'lashes_br_price_list';
+    $DATABASE_NAME = 'lashesbr_price_list';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
--- admin/index.php
+++ admin/index.php
@@ -22,9 +22,9 @@ if (!isset($_SESSION['loggedin'])) {
 // We need to use sessions, so you should always start sessions using the below code.
 
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'admin';
+$DATABASE_USER = 'ashesbr_admin';
+$DATABASE_PASS = 'LBA_2021!';
+$DATABASE_NAME = 'lashesbr_price_list';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
