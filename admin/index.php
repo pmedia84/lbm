db3de033a5353b1d914a495019750f0fb930773d
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-11-22 14:49:07 +0000

    .

--- admin/index.php
+++ admin/index.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION['loggedin'])) {
 // We need to use sessions, so you should always start sessions using the below code.
 
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'ashesbr_admin';
+$DATABASE_USER = 'lashesbr_admin';
 $DATABASE_PASS = 'LBA_2021!';
 $DATABASE_NAME = 'lashesbr_price_list';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
