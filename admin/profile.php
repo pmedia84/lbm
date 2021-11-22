266012a1f1197ebca524c7610edb924fddf5f5c3
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-11-22 14:55:15 +0000

    .

--- admin/profile.php
+++ admin/profile.php
@@ -1,12 +1,9 @@
-
-
-
 <?php
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
-	header('Location: index.html');
+	header('Location: index.php');
 	exit;
 }
 $DATABASE_HOST = 'localhost';
