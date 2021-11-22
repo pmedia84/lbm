51c295a86521288361a337acc8a40b0af9be591b
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-11-22 14:53:35 +0000

    .

--- admin/profile.php
+++ admin/profile.php
@@ -10,9 +10,9 @@ if (!isset($_SESSION['loggedin'])) {
 	exit;
 }
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'admin';
+$DATABASE_USER = 'lashesbr_admin';
+$DATABASE_PASS = 'LBA_2021!';
+$DATABASE_NAME = 'lashesbr_price_list';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
