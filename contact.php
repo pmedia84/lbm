4bc01f31645062915f1bcd494a61abd1bf46c8a9
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-12-16 14:34:21 +0000

    ...

--- contact.php
+++ contact.php
@@ -140,8 +140,7 @@
         <?php include("php/map.inc.php"); ?>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+
     <script type="text/javascript">
         //set vars for checking status of recaptcha
         var recaptchaCheck = false;
