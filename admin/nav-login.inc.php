696691f55ac3122f2c75923a5a797b7709efd131
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-12-17 11:45:03 +0000

    dfgfg

--- admin/nav-login.inc.php
+++ admin/nav-login.inc.php
@@ -5,12 +5,6 @@
 <meta name="theme-color" content="#f9d4d2">
 <meta name="keywords" content="lashes, brows, aesthetics, Long Sutton, Lincolnshire, eyelash extensions, dermaplaning">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta property="og:title" content= "$title - Beauty" />
-<meta property="og:description" content="Transform your eyelashes to be fuller, longer and curlier for a sensational look" />
-<meta property="og:image" content="https://lashesandbrowsbymandi.co.uk/og-data/logo.png"/>
-<meta property="og:type" content="website" />
-
-
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Pinyon+Script&family=Playfair+Display&display=swap" rel="stylesheet">
@@ -18,7 +12,7 @@
 <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
 <link rel="manifest" href="site.webmanifest">
-<link rel="stylesheet" href="css/flickity.css">
+
 <link rel="stylesheet" href="../css/styles.css">
 <script src="https://kit.fontawesome.com/3318fdaaaf.js" crossorigin="anonymous"></script>
 </head>
