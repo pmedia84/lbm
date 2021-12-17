459504d9454604b273bb3b4de5e70121d8d6c013
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-12-17 13:42:25 +0000

    dasds

--- php/pwsbranding.inc.php
+++ php/pwsbranding.inc.php
@@ -2,7 +2,7 @@
 
 
 <div class="pmbrand p-top-1">
-    <p class="copyright">&copy; Lashes Brows and Aesthetics 2021 </p><br>
+    <p class="copyright">&copy; Lashes Brows and Aesthetics <?php echo date("Y"); ?> </p><br>
     <a href="privacy">Privacy Notice</a>
     <p class="pm-branding">Website Created by <a href="http://www.parrotmedia.co.uk" target="_blank"><span class="parrotname">Parrot</span><span class="medianame"> Media</span></a></p>
 </div>
