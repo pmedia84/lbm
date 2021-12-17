27a4a7755ccc4551bc7b5664f663d85b36148b32
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2021-12-17 11:44:02 +0000

    hfghgh

--- admin/nav-admin.inc.php
+++ admin/nav-admin.inc.php
@@ -18,7 +18,7 @@
 <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
 <link rel="manifest" href="site.webmanifest">
-<link rel="stylesheet" href="css/flickity.css">
+
 <link rel="stylesheet" href="../css/styles.css">
 <script src="https://kit.fontawesome.com/3318fdaaaf.js" crossorigin="anonymous"></script>
 </head>
